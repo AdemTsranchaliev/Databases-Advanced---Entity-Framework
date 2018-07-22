0bb3ad08e6f05e2a3e801799adb1f12c02fa714b
Adem <ademcran4aliev@abv.bg>
2018-07-22 23:46:45 +0300

    code-first

---
+++ Code-First/P01_HospitalDatabase/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using P01_HospitalDatabase.Data;
+using P01_HospitalDatabase.Data.Models;
+using P01_HospitalDatabase.Initializer;
+using System;
+
+namespace P01_HospitalDatabase
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            using (HospitalContext context= new HospitalContext())
+            {
+                DatabaseInitializer.InitialSeed(context);
+               
+            }
+        }
+    }
+}
